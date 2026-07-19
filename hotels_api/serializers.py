eb974369aaa6e6685dd75e037004bf3841d0692a
PiyushYadav15 <piyush1234727@gmail.com>
2026-07-19 16:57:39 +0530

    Delete hotels_api directory

--- hotels_api/serializers.py
+++
@@ -1,9 +0,0 @@
-# hotels/serializers.py
-
-from rest_framework import serializers
-from .models import Hotel
-
-class HotelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Hotel
-        fields = '__all__'
